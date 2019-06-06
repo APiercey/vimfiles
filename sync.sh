6cdefd206896b0aeec7b72f262105d669c721dfd
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2019-06-06 22:10:12 +0200

    update

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 (ln -s ~/vimfiles/.vimrc ~/)
 (ln -s ~/vimfiles/.vim ~/)
 (ln -s ~/vimfiles/.config/coc ~/.config)
-(ln -s ~/vimfiles/.local/nvim ~/.local/nvim)
+(ln -s ~/vimfiles/.config/nvim ~/.config)
+(ln -s ~/vimfiles/.local/share/nvim ~/.local/share)
