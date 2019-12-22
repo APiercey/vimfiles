1de530ca11b3835adb570baa4a0cde738af68029
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2019-12-22 23:35:00 +0100

    overwrite

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
-(ln -s ~/vimfiles/.vim ~/)
-(ln -s ~/vimfiles/.config/coc ~/.config)
-(ln -s ~/vimfiles/.config/nvim ~/.config)
-(ln -s ~/vimfiles/.local/share/nvim ~/.local/share)
+mkdir ~/.local
+mkdir ~/.config
+mkdir ~/.local/share/nvim
+
+(ln -sf ~/vimfiles/.vim ~/)
+(ln -sf ~/vimfiles/.config/coc ~/.config)
+(ln -sf ~/vimfiles/.config/nvim ~/.config)
+(ln -sf ~/vimfiles/.local/share/nvim ~/.local/share)
