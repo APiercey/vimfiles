64220f6a2816cfb2dcad22a5f8c0d839abc22733
Alexander Piercey <alexander.piercey@gmail.com>
2021-01-17 14:08:36 +0100

    Remove share

--- sync.sh
+++ sync.sh
@@ -1,8 +1,5 @@
-mkdir -p ~/.local
 mkdir -p ~/.config
-mkdir -p ~/.local/share/nvim
 
 (ln -sf ~/vimfiles/.vim ~/)
 (ln -sf ~/vimfiles/.config/coc ~/.config)
 (ln -sf ~/vimfiles/.config/nvim ~/.config)
-(ln -sf ~/vimfiles/.local/share/nvim ~/.local/share)
